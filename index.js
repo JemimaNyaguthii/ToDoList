6f9c1d6dd654ab79140844e38ec5c63a46d6173b
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-07-06 07:28:05 +0300

    Update changes

--- index.js
+++ index.js
@@ -1,39 +1,82 @@
 
-const toDoList = document.getElementById("toDoList");
-const addTask = document.getElementById("addButton");
-const taskInput = document.getElementById("taskInput");
-// const deleteButton = document.getElementById("deleteButton");
+const userContainer = document.getElementById('todos');
+const taskIdInput= document.getElementById('taskInput');
+const getUsers = async () => {
+  try {
+    const response = await fetch('https://dummyjson.com/todos?limit=30');
+    if (!response.ok) {
+      throw new Error('Failed to fetch users');
+    }
+    const data = await response.json();
+    return data.todos;
+  } catch (error) {
+    console.log(error);
+  }
+};
+const displayUsers = async () => {
+  const users = await getUsers();
+  console.log(users);
+  if (Array.isArray(users)) {
+    users.forEach(item => {
+      let li = document.createElement('li');
+      let checkbox = document.createElement('input');
+      let label = document.createElement('taskInput');
+      let deleteButton = document.createElement('button');
+      li.style.display = 'flex';
+      checkbox.type = 'checkbox';
+      checkbox.style.marginRight = '10px';
+      label.textContent = item.todo;
+      deleteButton.textContent = 'X';
+      deleteButton.classList.add('delete-button');
+      deleteButton.addEventListener('click', () => {
+        deleteTask(item.id);
+        li.remove();
+      });
+      li.appendChild(checkbox);
+      li.appendChild(label);
+      li.appendChild(deleteButton);
+      userContainer.appendChild(li);
+    });
+  }
+};
 
-addTask.addEventListener("click", function() {
-  const task = taskInput.value;
-  toDoList.innerHTML += `<p>${task}</p>`;
-  taskInput.value = "";
-});
+const deleteTask = async (taskId) => {
+  try {
+    const response = await fetch(`https://dummyjson.com/todos/add`, {
+      method: 'DELETE'
+    });
+    if (!response.ok) {
+      throw new Error('Failed to delete task');
+    }
+  } catch (error) {
+    console.log(error);
+  }
 
-
-const todoItem = document.getElementById('todo-item');
-const todoText = todoItem.textContent;
-console.log(todoText);
-fetch('https://dummyjson.com/todos')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
-
-
-  fetch('https://dummyjson.com/todos/add', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    todo: 'Bike riding',
-    completed: false,
-    userId: 30,
-  })
-})
-
-
-
-
-
-
+};
+displayUsers();
+const addNewTask = () => {
+  const taskInput = document.getElementById('addButton');
+  const newTask = taskInput.value.trim();
+  taskInput.value = '';
+  if (newTask) {
+    let li = document.createElement('li');
+    let checkbox = document.createElement('input');
+    let label = document.createElement('taskInput');
+    let deleteButton = document.createElement('button');
+    li.style.display = 'flex';
+    checkbox.type = 'checkbox';
+    checkbox.style.marginRight = '10px';
+    label.textContent = newTask;
+    deleteButton.textContent = 'X';
+    deleteButton.classList.add('delete-button');
+    deleteButton.addEventListener('click', () => {
+      li.remove();
+    });
+    li.appendChild(checkbox);
+    li.appendChild(label);
+    li.appendChild(addButton);
+    userContainer.appendChild(li);
+  }
+};
 
 
